1b3c20e61a425fb5f6c6027bc6cc5a90a0ae7f4a
yashnak <yashna.kumar@gmail.com>
2018-01-03 23:59:14 -0800

    Update q1.java

--- src/q1.java
+++ src/q1.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by renuka on 11/27/17.
+ * Created by yashna on 11/27/17.
  */
 public class q1 {
     public static void main(String[] args) throws IOException {
